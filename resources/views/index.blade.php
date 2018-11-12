c03abde80b26794f6b793b4bafcb468b7f76e9cb
Taylor Le Fustec <taylorelliott96@icloud.com>
2018-11-12 23:30:36 +0000

    index front end development

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,32 @@
 
 @section('content')
 
-    <h1>tay<span>.</span></h1>
-
-    <small class="text1 text-muted">Buy, Sell, Strum, Code</small>
-    <small class="text2 text-muted">Zombie Killer Robots Inc.</small>
+    <div class="mycontainer">
+        <div class="top">
+            <h1>tay<span>.</span></h1>
+            <div><p class="text1 text-muted">Buy, Sell, Strum, Code</p></div>
+            <div><p class="text2 text-muted">Zombie Killer Robots Inc.</p></div>
+        </div>
+        
+        <div class="bottom">
+            <div class="card-container">
+                <div class="mycard">
+                    <div class="card-portfolio">
+                        Portfolio
+                    </div>
+                </div>
+                <div class="mycard">
+                    <div class="card-contact">
+                        Contact
+                    </div>
+                </div>
+                <div class="mycard">
+                    <div class="card-blog">
+                        Blog
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     
 @endsection
