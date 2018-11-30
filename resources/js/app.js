5749c8628ab3b8e65c0d77e662f5b193cf0dd0f7
Taylor Le Fustec <taylorelliott96@icloud.com>
2018-11-30 23:46:30 +0000

    Pre live merge clean

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,29 +8,17 @@ require("./bootstrap");
 
 $(document).ready(function() {
     console.log("ready");
-
-<<<<<<< HEAD
-    var small = $("small");
-    var clicks = 0;
-=======
     let slogan = $(".slogan");
     let clicks = 0;
     let clickLog = 0;
->>>>>>> develop
 
     slogan.click(function() {
         clicks++;
-<<<<<<< HEAD
-        small.text("Zombie Killer Robots Inc.").css("right", "38px");
-        if (clicks == 2) {
-            small.text("Buy, Sell, Strum, Code.").css("right", "32px");
-=======
         clickLog++;
         console.log(clicks + ", " + clickLog);
         slogan.text("Zombie Killer Robots Inc.").css("right", "38px");
         if (clicks == 2) {
             slogan.text("Buy, Sell, Strum, Code.").css("right", "32px");
->>>>>>> develop
             clicks = 0;
         }
         // Easter egg: 1
